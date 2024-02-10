39c8a2b5e10cb30f7f23bcdbf3613daabe1b0de2
Wiola-D <wioletta.d.work@gmail.com>
2024-02-10 10:20:41 +0100

    add color-witcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('.js-start');
+const btnStop = document.querySelector('.js-stop');
+const bg = document.querySelector('body');
+let timerId = null;
+btnStop.disabled = true;
 
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bg.style.background = getRandomHexColor();
+  }, 1000);
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
